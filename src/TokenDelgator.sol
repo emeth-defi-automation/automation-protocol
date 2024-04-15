7f12f6cb821a39432b984d94112121a132153d06
Neo <neotheprogramist@proton.me>
2024-04-15 09:58:05 +0200

    batch transfer impl

--- src/TokenDelgator.sol
+++ src/TokenDelgator.sol
@@ -5,7 +5,15 @@ import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract TokenDelegator {
     mapping(address => mapping(address => bool)) public approvals;
-    
+
+    // Define the Transfer struct
+    struct Transfer {
+        IERC20 token;
+        address from;
+        address to;
+        uint256 amount;
+    }
+
     function approve(address _user) public {
         approvals[_user][msg.sender] = true;
     }
@@ -23,5 +31,14 @@ contract TokenDelegator {
         require(approvals[msg.sender][_from], "TokenDelegator: not approved");
         token.transferFrom(_from, address(this), _amount);
         token.transfer(_to, _amount);
+    }
+
+    function transferBatch(Transfer[] memory transfers) public {
+        for (uint256 i = 0; i < transfers.length; i++) {
+            Transfer memory t = transfers[i];
+            require(approvals[msg.sender][t.from], "TokenDelegator: not approved for all tokens");
+            t.token.transferFrom(t.from, address(this), t.amount);
+            t.token.transfer(t.to, t.amount);
+        }
     }
 }
--- test/TokenDelegator.t.sol
+++ test/TokenDelegator.t.sol
@@ -53,4 +53,54 @@ contract TokenDelegatorTest is Test {
         vm.expectRevert();
         tokenDelegator.transferToken(token, from, to, transferAmount);
     }
+
+    function testBatchTransfer() public {
+        TokenDelegator.Transfer[] memory transfers = new TokenDelegator.Transfer[](2);
+        transfers[0] = TokenDelegator.Transfer(token, from, to, 100);
+        transfers[1] = TokenDelegator.Transfer(token, from, to, 200);
+
+        // Setup approvals and token allowances
+        vm.prank(from);
+        tokenDelegator.approve(user); // from approves user to manage their tokens
+        vm.prank(from);
+        token.approve(address(tokenDelegator), 300); // from approves TokenDelegator to handle 300 tokens
+
+        uint256 initialBalanceTo = token.balanceOf(to);
+        uint256 initialBalanceFrom = token.balanceOf(from);
+
+        // Execute batch transfer
+        vm.prank(user);
+        tokenDelegator.transferBatch(transfers);
+
+        // Check balances after transfer
+        assertEq(token.balanceOf(to), initialBalanceTo + 300); // to should receive 300 tokens total
+        assertEq(token.balanceOf(from), initialBalanceFrom - 300); // from should have 300 less tokens
+    }
+
+    function testBatchTransferWithoutApproval() public {
+        TokenDelegator.Transfer[] memory transfers = new TokenDelegator.Transfer[](2);
+        transfers[0] = TokenDelegator.Transfer(token, from, to, 100);
+        transfers[1] = TokenDelegator.Transfer(token, from, to, 200);
+
+        // Expecting a revert due to lack of approval
+        vm.expectRevert("TokenDelegator: not approved for all tokens");
+        vm.prank(user);
+        tokenDelegator.transferBatch(transfers);
+    }
+
+    function testBatchTransferInsufficientBalance() public {
+        TokenDelegator.Transfer[] memory transfers = new TokenDelegator.Transfer[](1);
+        transfers[0] = TokenDelegator.Transfer(token, from, to, 1100); // Amount greater than the balance
+
+        // Setup approvals
+        vm.prank(from);
+        tokenDelegator.approve(user);
+        vm.prank(from);
+        token.approve(address(tokenDelegator), 1100); // Attempt to approve more than the balance
+
+        // Expecting a revert due to insufficient balance
+        vm.expectRevert();
+        vm.prank(user);
+        tokenDelegator.transferBatch(transfers);
+    }
 }
