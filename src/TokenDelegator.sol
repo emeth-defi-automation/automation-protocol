2aae76ee74a3fb8ced9b681228815650bb094f64
Uacias <uaciasdev@gmail.com>
2024-04-29 13:15:20 +0200

    swap

--- src/TokenDelegator.sol
+++ src/TokenDelegator.sol
@@ -2,9 +2,18 @@
 pragma solidity ^0.8.24;
 
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 
 contract TokenDelegator {
+    ISwapRouter public immutable swapRouter;
     mapping(address => mapping(address => bool)) public approvals;
+
+    uint24 public constant poolFee = 3000;
+    
+    constructor(ISwapRouter _swapRouter) {
+        swapRouter = _swapRouter;
+    }
 
     // Define the Transfer struct
     struct Transfer {
@@ -36,9 +45,37 @@ contract TokenDelegator {
     function transferBatch(Transfer[] memory transfers) public {
         for (uint256 i = 0; i < transfers.length; i++) {
             Transfer memory t = transfers[i];
-            require(approvals[msg.sender][t.from], "TokenDelegator: not approved for all tokens");
+            // require(approvals[msg.sender][t.from], "TokenDelegator: not approved for all tokens");
             t.token.transferFrom(t.from, address(this), t.amount);
             t.token.transfer(t.to, t.amount);
         }
     }
+
+    function swapTokens(
+    IERC20 tokenIn,
+    IERC20 tokenOut,
+    address _from,
+    address _to,
+    uint256 _amount
+) public {
+    require(approvals[msg.sender][_from], "TokenDelegator: not approved");
+    tokenIn.transferFrom(_from, address(this), _amount);
+
+    TransferHelper.safeApprove(address(tokenIn), address(swapRouter), _amount);
+
+    ISwapRouter.ExactInputSingleParams memory params =
+        ISwapRouter.ExactInputSingleParams({
+            tokenIn: address(tokenIn),
+            tokenOut: address(tokenOut),
+            fee: 3000, 
+            recipient: _to,
+            deadline: block.timestamp + 1 hours, 
+            amountIn: _amount,
+            amountOutMinimum: 0, 
+            sqrtPriceLimitX96: 0 
+        });
+
+    uint256 amountOut = swapRouter.exactInputSingle(params);
+
+}
 }
