b6426e445220e45e0c06dc0c3c578c666831ee5c
Uacias <uaciasdev@gmail.com>
2024-04-29 13:38:49 +0200

    fmt

--- src/TokenDelegator.sol
+++ src/TokenDelegator.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.24;
 
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 
 contract TokenDelegator {
     ISwapRouter public immutable swapRouter;
     mapping(address => mapping(address => bool)) public approvals;
 
     uint24 public constant poolFee = 3000;
-    
+
     constructor(ISwapRouter _swapRouter) {
         swapRouter = _swapRouter;
     }
@@ -26,17 +26,12 @@ contract TokenDelegator {
     function approve(address _user) public {
         approvals[_user][msg.sender] = true;
     }
-    
+
     function allowance(address _owner, address _spender) public view returns (bool) {
         return approvals[_owner][_spender];
     }
 
-    function transferToken(
-        IERC20 token,
-        address _from,
-        address _to,
-        uint256 _amount
-    ) public {
+    function transferToken(IERC20 token, address _from, address _to, uint256 _amount) public {
         require(approvals[msg.sender][_from], "TokenDelegator: not approved");
         token.transferFrom(_from, address(this), _amount);
         token.transfer(_to, _amount);
@@ -51,31 +46,23 @@ contract TokenDelegator {
         }
     }
 
-    function swapTokens(
-    IERC20 tokenIn,
-    IERC20 tokenOut,
-    address _from,
-    address _to,
-    uint256 _amount
-) public {
-    require(approvals[msg.sender][_from], "TokenDelegator: not approved");
-    tokenIn.transferFrom(_from, address(this), _amount);
+    function swapTokens(IERC20 tokenIn, IERC20 tokenOut, address _from, address _to, uint256 _amount) public {
+        require(approvals[msg.sender][_from], "TokenDelegator: not approved");
+        tokenIn.transferFrom(_from, address(this), _amount);
 
-    TransferHelper.safeApprove(address(tokenIn), address(swapRouter), _amount);
+        TransferHelper.safeApprove(address(tokenIn), address(swapRouter), _amount);
 
-    ISwapRouter.ExactInputSingleParams memory params =
-        ISwapRouter.ExactInputSingleParams({
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
             tokenIn: address(tokenIn),
             tokenOut: address(tokenOut),
-            fee: 3000, 
+            fee: 3000,
             recipient: _to,
-            deadline: block.timestamp + 1 hours, 
+            deadline: block.timestamp + 1 hours,
             amountIn: _amount,
-            amountOutMinimum: 0, 
-            sqrtPriceLimitX96: 0 
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0
         });
 
-    swapRouter.exactInputSingle(params);
-
-}
+        swapRouter.exactInputSingle(params);
+    }
 }
