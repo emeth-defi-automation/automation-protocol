e67c4aca9fa18f1d5ba939be9d5a1b7fea16cab9
pgajek <pgajek94@gmail.com>
2024-06-06 19:17:52 +0200

    swap automation add action

--- src/SwapAutomation.sol
+++ src/SwapAutomation.sol
@@ -2,6 +2,38 @@
 pragma solidity ^0.8.0;
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
+interface IUniswapV2Router {
+    function WETH() external pure returns (address);
+
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+
+    function swapExactTokensForETH(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+
+    function swapExactETHForTokens(
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable returns (uint[] memory amounts);
+
+    function getAmountsOut(
+        uint amountIn,
+        address[] calldata path
+    ) external view returns (uint[] memory amounts);
+}
+
 contract SwapAutomation {
     struct SwapAction {
         address ownerAddress;
@@ -18,4 +50,33 @@ contract SwapAutomation {
 
     mapping(uint => SwapAction) public actions;
     uint[] public actionIds;
+
+    IUniswapV2Router public uniswapV2Router;
+
+    constructor() {
+        uniswapV2Router = IUniswapV2Router(
+            0x87aE49902B749588c15c5FE2A6fE6a1067a5bea0
+        );
+    }
+    function uintToBool(uint value) public pure returns (bool) {
+        return value != 0;
+    }
+    function addAction(uint actionId, uint action) public returns (uint) {
+        require(!actions[actionId].initialized, "Action ID already exists");
+
+        actions[actionId] = SwapAction({
+            ownerAddress: address(action[0]),
+            initialized: action[1], // should convert to bool be used or rather 0 and 1
+            duration: action[2],
+            timeZero: action[3],
+            tokenIn: address(action[4]),
+            tokenOut: address(action[5]),
+            amountIn: action[6],
+            from: address(action[7]),
+            to: address(action[8]),
+            isActive: action[9] //same
+        });
+        actionIds.push(actionId);
+        return actionId;
+    }
 }
--- src/TransferAutomation.sol
+++ src/TransferAutomation.sol
@@ -21,4 +21,19 @@ contract TransferAutomation {
 
     mapping(uint => TransferAction) public actions;
     uint[] public actionIds;
+
+    function addAction(uint actionId, uint[] action) public returns (uint) {
+        require(!actions[actionId].initialized, "Action ID already exists");
+
+        actions[actionId] = TransferAction({
+            ownerAddress: action[0],
+            initialized: action[1],
+            duration: action[2],
+            timeZero: action[3],
+            isActive: action[4]
+            // transfers:
+        });
+        actionIds.push(actionId);
+        return actionId;
+    }
 }
