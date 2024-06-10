141c98bb6a20397d2913a50c9c722ca1629e65c1
pgajek <pgajek94@gmail.com>
2024-06-10 13:20:48 +0200

    deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,24 @@
-#!/usr/bin/env bash
-source .env && \
-forge create --legacy --rpc-url $ALCHEMY --private-key $PRIVATE_KEY src/TokenDelegator.sol:TokenDelegator && \
-deactivate
+#!/bin/bash
+
+# Run the first script
+./scripts/deployTransfer.sh
+if [ $? -ne 0 ]; then
+  echo "./scripts/deployTransfer.sh failed"
+  exit 1
+fi
+
+# Run the second script
+./scripts/deploySwap.sh
+if [ $? -ne 0 ]; then
+  echo "./scripts/deploySwap.sh failed"
+  exit 1
+fi
+
+# Run the third script
+./scripts/deployMain.sh
+if [ $? -ne 0 ]; then
+  echo "./scripts/deployMain.sh failed"
+  exit 1
+fi
+
+echo "All scripts ran successfully"
